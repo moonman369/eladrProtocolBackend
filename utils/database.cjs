b86ed80c21a43b995049f9d24091cc4a26a940f2
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2022-10-21 11:06:05 +0530

    Commit: Cardano address: addr1qxz5kymdh0j49emj5dzjpv7mdnacdzvmahefadsp3rkypkll54hl9gsny78lpvlvc2ktx6ww3zhysuja7p3fyzs0zjzq4dxest
    Message: Completed installation process for windows. Using phpMyAdmin

--- utils/database.cjs
+++ utils/database.cjs
@@ -1,20 +1,15 @@
 //var mysql = require('mysql');
-require('dotenv').config();
-const { Sequelize } = require('sequelize');
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
 
 let dbhost = process.env.DB_HOST;
-let dbuser=process.env.DB_USER;
+let dbuser = process.env.DB_USER;
 let dbpassword = process.env.DB_PASSWORD;
 let databaseName = process.env.DB_DATABASENAME;
-console.log('db name:',dbhost);
-const sequelize = new Sequelize(
-    databaseName,
-    dbuser,
-    dbpassword,
-     {
-       host: dbhost,
-       dialect: 'mysql'
-     }
-);
+console.log("db name:", databaseName);
+const sequelize = new Sequelize(databaseName, dbuser, dbpassword, {
+  host: dbhost,
+  dialect: "mysql",
+});
 
 module.exports = sequelize;
